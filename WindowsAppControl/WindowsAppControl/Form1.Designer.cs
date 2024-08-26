29acb5128962eafc09490bf06b0300ffe46df783
Allan Barcelos <allan@barcelos.dev>
2024-08-26 07:51:03 -0400

    windows app

---
+++ WindowsAppControl/WindowsAppControl/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsAppControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private void InitializeComponent()
+        {
+            this.textBoxCommand = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonSendCommand = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxCommand
+            // 
+            this.textBoxCommand.Location = new System.Drawing.Point(12, 12);
+            this.textBoxCommand.Name = "textBoxCommand";
+            this.textBoxCommand.Size = new System.Drawing.Size(776, 20);
+            this.textBoxCommand.TabIndex = 0;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 38);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(776, 371);
+            this.textBoxOutput.TabIndex = 1;
+            // 
+            // buttonSendCommand
+            // 
+            this.buttonSendCommand.Location = new System.Drawing.Point(713, 415);
+            this.buttonSendCommand.Name = "buttonSendCommand";
+            this.buttonSendCommand.Size = new System.Drawing.Size(75, 23);
+            this.buttonSendCommand.TabIndex = 2;
+            this.buttonSendCommand.Text = "Send";
+            this.buttonSendCommand.UseVisualStyleBackColor = true;
+            this.buttonSendCommand.Click += new System.EventHandler(this.buttonSendCommand_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSendCommand);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxCommand);
+            this.Name = "Form1";
+            this.Text = "Remote Control Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBoxCommand;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonSendCommand;
+    }
+}
+
---
+++ WindowsAppControl/WindowsAppControl/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace WindowsAppControl
+{
+    public partial class Form1 : Form
+    {
+        private WebSocket _webSocket;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeWebSocket();
+        }
+
+        private void InitializeWebSocket()
+        {
+            string serverUrl = Environment.GetEnvironmentVariable("SERVER_URL"); // "ws://0.0.0.0:8080";
+            _webSocket = new WebSocket(serverUrl);
+
+            _webSocket.OnOpen += (sender, e) =>
+            {
+                // Envie a autenticação quando a conexão estiver aberta
+                SendAuthentication();
+            };
+
+            _webSocket.OnMessage += (sender, e) =>
+            {
+                Invoke((MethodInvoker)(() => OnMessageReceived(e.Data)));
+            };
+
+            _webSocket.OnError += (sender, e) =>
+            {
+                Invoke((MethodInvoker)(() => OnMessageReceived($"Error: {e.Message}")));
+            };
+
+            _webSocket.OnClose += (sender, e) =>
+            {
+                if (this.IsHandleCreated)
+                {
+                    Invoke((MethodInvoker)(() => OnMessageReceived("Connection closed")));
+                }
+            };
+
+            _webSocket.Connect();
+        }
+
+        private void SendAuthentication()
+        {
+            string token = Environment.GetEnvironmentVariable("TOKEN");
+            string authMessage = $"{{\"token\": \"{token}\"}}";
+            if (_webSocket.ReadyState == WebSocketState.Open)
+            {
+                _webSocket.Send(authMessage);
+            }
+            else
+            {
+                // Tratar o caso onde o WebSocket não está aberto
+                Invoke((MethodInvoker)(() => OnMessageReceived("WebSocket is not open. Failed to send authentication.")));
+            }
+        }
+
+        private void OnMessageReceived(string message)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)(() => textBoxOutput.AppendText($"{message}\r\n")));
+            }
+            else
+            {
+                textBoxOutput.AppendText($"{message}\r\n");
+            }
+        }
+
+        private void buttonSendCommand_Click(object sender, EventArgs e)
+        {
+            string command = textBoxCommand.Text;
+            if (!string.IsNullOrEmpty(command))
+            {
+                _webSocket.Send(command);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _webSocket.Close();
+        }
+    }
+}
